2c0eeacb68baae07e949531c059ac16dc348a8ec
Adam2001-sun <83719329+Adam2001-sun@users.noreply.github.com>
2022-04-25 03:35:44 +0930

    Add files via upload

--- MapGeneric.h
+++ MapGeneric.h
@@ -1,5 +1,5 @@
 //
-// Created by Sun Haoxian on 22/4/22.
+// Created by Sun Haoxian on 21/4/22.
 //
 
 #ifndef CMAKE_CLION_CONAN_TEMPLATE_MAPGENERIC_H
--- ReduceMinimum.cpp
+++ ReduceMinimum.cpp
@@ -1,5 +1,5 @@
 //
-// Created by Sun Haoxian on 25/4/22.
+// Created by Sun Haoxian on 22/4/22.
 //
 
 #include "ReduceMinimum.h"
@@ -9,7 +9,7 @@ ReduceMinimum::ReduceMinimum() = default;
 
 
 int ReduceMinimum::binaryOperator(int x, int y) {
-    
+
     if(x>y){
         return y;
     }else
